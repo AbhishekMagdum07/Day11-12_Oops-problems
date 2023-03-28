192c1fc59307cf5d60a2fb06983d50e4cf396af3
Abhishek <abhishek.magdum.07@gmail.com>
2023-03-28 10:53:08 +0530

    Added: Deck of Cards practice problem

--- src/com/oops_problems/DeckOfCards.java
+++ src/com/oops_problems/DeckOfCards.java
@@ -1,29 +1,65 @@
 package com.oops_problems;
 
+import java.util.Random ;
+import java.util.Arrays;
 public class DeckOfCards {
-    public static void deckOfCards() {
-        int[] deck = new int[52];
-        String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
-        String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+    Random random = new Random();
+    String[] suits = {"Clubs", "Diamond", "Hearts", "Spade"};
+    String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+    int numberofCards = suits.length * rank.length; // 52
+    String[][] cards = new String[suits.length][rank.length];
 
-
-        for (int i = 0; i < deck.length; i++) { // initializing the cards
-            deck[i] = i;
-        }
-
-        for (int i = 0; i < deck.length; i++) { // shuffle the cards
-            int index = (int) (Math.random() * deck.length);
-            int temp = deck[i];
-            deck[i] = deck[index];
-            deck[index] = temp;
-        }
-        for (int i = 0; i < 52; i++) {
-            String suit = suits[deck[i] / 13];
-            String rank = ranks[deck[i] % 13];
-            System.out.println(rank + " of " + suit);
+    public void initializingDeck() {
+        for (int i = 0; i < suits.length; i++) { //loop for suits
+            for (int j = 0; j < rank.length; j++) { // loop for rank
+                cards[i][j] = suits[i] + " " + rank[j];
+            }
         }
     }
+    public void suffleDeck() {
+        for (int i = 0; i < cards.length; i++) {
+            for (int j = 0; j < cards[i].length; j++) {
+                int x = random.nextInt(3);
+                int y = random.nextInt (12);
+                String temp = cards[i][j];
+                cards[i][j] = cards[x][y];
+                cards[x][y] = temp;
+            }
+        }
+    }
+
+    int limitOfCards = 9;
+    String[] player1 = new String[limitOfCards];
+    String[] player2 = new String[limitOfCards];
+    String[] player3 = new String[limitOfCards];
+    String[] player4 = new String[limitOfCards];
+    String[] card = new String[numberofCards];
+
+    public void distributeToPlayers() {
+        int index = 0;
+        for (int x = 0; x < suits.length; x++) {
+            for (int y = 0; y < rank.length; y++) {
+                card[index++] = cards[x][y];
+            }
+        }
+        int j = 0;
+
+        for (int i = 0; i < limitOfCards; i++) {
+            player1[i] = card[j++];
+            player2[i] = card[j++];
+            player3[i] = card[j++];
+            player4[i] = card[j++];
+        }
+        System.out.println("Player 1 = " + Arrays.toString(player1));
+        System.out.println("Player 2 = " + Arrays.toString(player2));
+        System.out.println("Player 3 = " + Arrays.toString(player3));
+        System.out.println("Player 4 = " + Arrays.toString(player4));
+    }
+
     public static void main(String[] args) {
-        deckOfCards();
+        DeckOfCards deckOfCards = new DeckOfCards();
+        deckOfCards.initializingDeck();
+        deckOfCards.suffleDeck();
+        deckOfCards.distributeToPlayers();
     }
 }
